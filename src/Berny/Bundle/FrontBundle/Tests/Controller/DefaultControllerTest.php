3bd72c3a60d5e96b5554e1bd07d389e444772964
Berny Cantos <be@rny.cc>
2014-02-16 14:24:13 +0100

    split test and add better assertion messages

--- src/Berny/Bundle/FrontBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Berny/Bundle/FrontBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,20 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testHome()
+    public function test_home_renders_successfully()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
-        $this->assertTrue($client->getResponse()->isSuccessful());
-        $this->assertCount(1, $crawler->filter('html head + body'));
-        $this->assertCount(1, $crawler->filter('form'));
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'Response is not successful');
+        $this->assertCount(1, $crawler->filter('html head + body'), 'Response content must include <html> <head> and <body> tags');
+        $this->assertCount(1, $crawler->filter('form[name=rnycc_urlshortener]'), 'Response must include url shortener form');
+    }
+
+    public function test_home_correct_url_generation()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
 
         $button = $crawler->selectButton('rnycc_urlshortener[save]');
         $form = $button->form();
         $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirection());
+        $this->assertTrue($client->getResponse()->isRedirection(), 'Must redirect before form processing');
 
         $client->followRedirect();
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'Response before form processing must be successful');
     }
 }

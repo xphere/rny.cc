6a08548d9bba4177f288f2cecfee3f1f9e4ccc11
Berny Cantos <be@rny.cc>
2014-04-08 16:15:28 +0200

    show urls after creation

--- src/Berny/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/Berny/Bundle/FrontBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Berny\Bundle\FrontBundle\Controller;
 
 use Berny\Bundle\FrontBundle\Entity;
+use Berny\Bundle\FrontBundle\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,6 +15,7 @@ class DefaultController extends Controller
 
         if ($form->handleRequest($request)->isValid()) {
             $url = $this->get('rnycc.url_shortener.handler')->process($form);
+            $this->addSuccessMessage($url);
 
             return $this->redirect($this->generateUrl('rnycc_frontpage'));
         }
@@ -26,5 +28,10 @@ class DefaultController extends Controller
     public function redirectAction(Entity\Url $url)
     {
         return $this->redirect($url->getUrl());
+    }
+
+    protected function addSuccessMessage(Model\Url $url)
+    {
+        $this->get('session')->getFlashBag()->add('success', $url);
     }
 }
